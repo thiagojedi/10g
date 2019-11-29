0f5b36812f6619b330ef892e68c83b4f235f497e
Thiago Jedi <thiagoa7@gmail.com>
2019-11-28 21:05:57 -0300

    Config files

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+
+module.exports = {
+  mode: process.env.NODE_ENV || "development",
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"],
+    alias: {
+      'react': 'preact'
+    }
+  },
+  module: {
+    rules: [{ test: /\.[tj]sx?$/, loader: "babel-loader" }]
+  }
+};
